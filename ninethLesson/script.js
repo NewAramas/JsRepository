4cb4e0f433240c12411b6304910aa9092bd66fbf
Margarita Rizhkovskaya <Margushanew@mail.ru>
2018-07-09 23:39:59 +0300

    two tasks

---
+++ ninethLesson/script.js
@@ -0,0 +1,15 @@
+// --первая задача--//
+function mid(a,b){
+    return ((a+b)/2)
+}
+
+// --вторая задача--//
+function pow(x,n){
+    var result=x;
+    for(var i=1; i<n; i++){
+        result=result*x;
+    }
+    return result;
+}
+
+// --третья задача--//
